33941a16691b583ebbd865ad3c95169fddc76f03
rukpet <rukpet@bigmir.net>
2015-12-15 00:26:16 +0200

    добавлены атрибуты для Entity and MVC, переназвано автосвойство DateTime? Create

--- ACTS.Core/Entities/News.cs
+++ ACTS.Core/Entities/News.cs
@@ -11,15 +11,27 @@ namespace ACTS.Core.Entities
 	public class News
 	{
 		[Key]
+		[HiddenInput(DisplayValue = false)]
 		public int NewsID { get; set; }
 
+		[Required(ErrorMessage = "Please enter a title")]
+		[StringLength(500, ErrorMessage = "Title: Length should not exceed 500 characters")]
 		public string Title { get; set; }
 
-		public DateTime? DataCreate { get; set; }
+		[HiddenInput(DisplayValue = false)]
+		public DateTime? Create { get; set; }
+
+		[HiddenInput(DisplayValue = false)]
+		public DateTime? Modified { get; set; }
 
 		public byte [] ImageData { get; set; }
 
 		[HiddenInput(DisplayValue = false)]
-		public string ImageMimeType { get; set; } 
+		public string ImageMimeType { get; set; }
+
+		[Required(ErrorMessage = "Please enter a description")]
+		[DataType(DataType.MultilineText)]
+		[AllowHtml]
+		public string Content { get; set; }
 	}
 }
