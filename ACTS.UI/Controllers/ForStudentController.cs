aaf7a81270f997a46adefd973071ce822a6304a5
inatoff <inatoff@gmail.com>
2015-12-20 02:54:44 +0200

    Добавлен контроллер ForStudent

---
+++ ACTS.UI/Controllers/ForStudentController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ACTS.UI.Controllers
+{
+    public class ForStudentController : Controller
+    {
+        // GET: ForStudent
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Specialities()
+        {
+            return View();
+        }
+
+        public ActionResult StudentRating()
+        {
+            return View();
+        }
+         
+        public ActionResult UsefulLinks()
+        {
+            return View();
+        }
+
+        public ActionResult Schedule()
+        {
+            return View();
+        }
+
+        public ActionResult Postgraduate()
+        {
+            return View();
+        }
+
+        public ActionResult CoursesAndProbations()
+        {
+            return View();
+        }
+    }
+}
